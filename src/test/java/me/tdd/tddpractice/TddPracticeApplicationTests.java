9f51bada5883288665aa5037d900fb74166213fe
nashs789 <nashs789@naver.com>
2024-07-26 23:41:39 +0900

    Feat: init TDD project

---
+++ src/test/java/me/tdd/tddpractice/TddPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tdd.tddpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
